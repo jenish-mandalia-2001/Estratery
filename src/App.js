f99e18dd7e027b07f52423ade1769db3e616f949
jenish-mandalia-2001 <68216875+jenish-mandalia-2001@users.noreply.github.com>
2022-11-11 00:47:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
-import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/Home';
 
 const App = () => {
   return (
-    <div className='mx-auto'  style={{backgroundColor:'#f8f4ff'}}>
+    <div className='mx-auto' style={{ backgroundColor: '#f8f4ff' }}>
       <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+      <Home />
       <Footer />
     </div>
   );
